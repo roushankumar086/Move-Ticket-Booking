3456836cafd7b3b015100090e81c7f733be31cb1
Roushan Kumar <mr.rkumar086@gmail.com>
2025-04-22 16:30:19 +0530

    Refactor service and controller to use UserRegistrationRequest DTO

--- src/main/java/com/example/mtb/dto/UserRegistrationRequest.java
+++ src/main/java/com/example/mtb/dto/UserRegistrationRequest.java
@@ -1,4 +1,44 @@
 package com.example.mtb.dto;
 
-public record UserRegistrationRequest() {
+import com.example.mtb.enums.Role;
+
+import java.time.LocalDate;
+
+public record UserRegistrationRequest(String userName,
+                                      String email,
+                                      String phoneNumber,
+                                      String password,
+                                      Role role,
+                                      LocalDate dateOfBirth
+                                      ) {
+
+    @Override
+    public String userName() {
+        return userName;
+    }
+
+    @Override
+    public String password() {
+        return password;
+    }
+
+    @Override
+    public String phoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String email() {
+        return email;
+    }
+
+    @Override
+    public LocalDate dateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public Role role() {
+        return role;
+    }
 }
--- src/main/java/com/example/mtb/service/UserService.java
+++ src/main/java/com/example/mtb/service/UserService.java
@@ -1,9 +1,10 @@
 package com.example.mtb.service;
 
+import com.example.mtb.dto.UserRegistrationRequest;
 import com.example.mtb.entity.UserDetails;
 import org.springframework.stereotype.Service;
 
 
 public interface UserService {
-    public UserDetails userRegister(UserDetails userDetails);
+    public UserDetails userRegister(UserRegistrationRequest userRegistrationRequest);
 }
